7754dbdd691eb85eb051da537de25bbaeb9275fb
wktdev <wktdev@gmail.com>
2017-06-04 19:12:28 -0700

    uncomplete checkpoint_5 ... blank screen and no errors

---
+++ app/scripts/controllers/CookieCtrl.js
@@ -0,0 +1,17 @@
+(function() {
+  function BlocChatCookies($cookies, $uibModal) {
+    var currentUser = $cookies.get('blocChatCurrentUser');
+    if (!currentUser || currentUser === '') {
+       $uibModal.open({
+        // Modal configuration object properties
+        templateUrl: '/templates/new_user_modal.html',
+        controller: 'UserModalCtrl',
+        controllerAs: 'usermodal'
+      })
+    }
+  }
+
+  angular
+    .module('blocChat',['ngCookies','ui.bootstrap'])
+    .run(['$cookies', '$uibModal', BlocChatCookies]);
+})();
---
+++ app/scripts/controllers/UserModalCtrl.js
@@ -0,0 +1,18 @@
+(function() {
+    function UserModalCtrl($cookies, $uibModalInstance) {
+        var userModal = this;
+        userModal.statusBar = "This name will appear when you send messages";
+        userModal.setUsername = function() {
+            if(userModal.username){
+              $cookies.put('blocChatCurrentUser', userModal.username);
+              $uibModalInstance.dismiss('cancel');
+            } else {
+              alert("Username should not be empty!");
+            }
+        };
+    };
+
+    angular
+        .module('blocChat',['ngCookies','ui.bootstrap'])
+        .controller('UserModalCtrl', ['$cookies', '$uibModalInstance', UserModalCtrl]);
+})();
